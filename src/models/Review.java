a812b2f102d7155d0c81af9733a71a084cc0a66f
Michael <apfelbaum24@gmail.com>
2014-04-14 15:24:28 +0200

    file parser + csv writer (before adding rapidminer repo)

--- src/models/Review.java
+++ src/models/Review.java
@@ -5,8 +5,145 @@ package models;
 
 /**
  * @author Michi
- *
+ * 
  */
 public class Review {
+
+	private String name;
+	private String beerID;
+	private String brewerID;
+	private double ABV; // TODO:What exactly does that mean?
+	private String style;
+	private int appearance;
+	private int aroma;
+	private int palate;
+	private int taste;
+	private int overall;
+	private int time;
+	private String profileName;
+	private String text;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getBeerID() {
+		return beerID;
+	}
+
+	public void setBeerID(String beerID) {
+		this.beerID = beerID;
+	}
+
+	public String getBrewerID() {
+		return brewerID;
+	}
+
+	public void setBrewerID(String brewerID) {
+		this.brewerID = brewerID;
+	}
+
+	public double getABV() {
+		return ABV;
+	}
+
+	public void setABV(double aBV) {
+		ABV = aBV;
+	}
+
+	public String getStyle() {
+		return style;
+	}
+
+	public void setStyle(String style) {
+		this.style = style;
+	}
+
+	public int getAppearance() {
+		return appearance;
+	}
+
+	public void setAppearance(int appearance) {
+		this.appearance = appearance;
+	}
+
+	public int getAroma() {
+		return aroma;
+	}
+
+	public void setAroma(int aroma) {
+		this.aroma = aroma;
+	}
+
+	public int getPalate() {
+		return palate;
+	}
+
+	public void setPalate(int palate) {
+		this.palate = palate;
+	}
+
+	public int getTaste() {
+		return taste;
+	}
+
+	public void setTaste(int taste) {
+		this.taste = taste;
+	}
+
+	public int getOverall() {
+		return overall;
+	}
+
+	public void setOverall(int overall) {
+		this.overall = overall;
+	}
+
+	public int getTime() {
+		return time;
+	}
+
+	public void setTime(int time) {
+		this.time = time;
+	}
+
+	public String getProfileName() {
+		return profileName;
+	}
+
+	public void setProfileName(String profileName) {
+		this.profileName = profileName;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	@Override
+	public String toString() {
+		return "Review [name=" + name + ", beerID=" + beerID + ", brewerID="
+				+ brewerID + ", ABV=" + ABV + ", style=" + style
+				+ ", appearance=" + appearance + ", aroma=" + aroma
+				+ ", palate=" + palate + ", taste=" + taste + ", overall="
+				+ overall + ", time=" + time + ", profileName=" + profileName
+				+ ", text=" + text + "]";
+	}
+	
+	public String toCSV() {
+		return name + "," + beerID + ","
+				+ brewerID + "," + ABV + "," + style
+				+ "," + appearance + "," + aroma
+				+ "," + palate + "," + taste + ","
+				+ overall + "," + time + "," + profileName
+				+ ",\"" + text + "\"";
+	}
 
 }
---
+++ src/test/ReadFileTest.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package test;
+
+import static org.junit.Assert.*;
+import models.Corpus;
+
+import org.junit.Test;
+
+/**
+ * @author Michi
+ *
+ */
+public class ReadFileTest {
+
+	@Test
+	public void test() {
+		Corpus co = new Corpus();
+		assertEquals(co.loadFromFile("data/first1000lines.txt"), 71);
+		co.writeToCSV("test.csv");
+	}
+
+}
