7790e43b2bfdc0af8a827974f6609069cdd432c9
Michi-4G <apfelbaum24@googlemail.com>
2014-05-03 15:57:28 +0200

    Refactored word list generator in own class

---
+++ src/experiments/GenerateWordLists.java
@@ -0,0 +1,60 @@
+/**
+ * 
+ */
+package experiments;
+
+import java.util.Properties;
+
+import models.Corpus;
+import models.WordListGenerator;
+
+/**
+ * @author Michi
+ *
+ */
+public class GenerateWordLists {
+
+
+	/**
+	 * Maximum number of reviews to load
+	 */
+	public static int topX = 2000;
+
+	/**
+	 * @param args
+	 * @throws Exception
+	 */
+	public static void main(String[] args) throws Exception {
+
+		// Setup properties here
+		Properties props = new Properties();
+		props.put("idfTransform", "true");
+		props.put("tfTransform", "true");
+		props.put("outputWordCounts", "true");
+		props.put("lowerCaseTokens", "true");
+		props.put("useStoplist", "true");
+		props.put("stopwordsFile", "data/english-stop-words-small.txt");
+		props.put("wordsToKeep", 2000);
+		props.put("minTermFreq", 3);
+
+		// Affects stanford core
+		props.put("posToKeep", "NN ADJ");// TODO not implemented yet (Marc) See
+											// Review.analyze()
+		props.put("useLemma", "true");// TODO:Check if lemma actually working
+		props.put("includePOS", "false");// TODO: Will mess up word vector
+											// creation
+		props.put("annotators", "tokenize, ssplit, pos, lemma");
+		Corpus co = new Corpus(props);
+
+		System.out.println(props.toString().replaceAll(", ", "\n"));
+
+		int size = co.loadFromFile("data/ratebeer.txt", topX);
+		System.out.println("Total amount of reviews loaded: " + size);
+		
+		WordListGenerator wlGenerator = new WordListGenerator("data/output/wordlists/");
+
+		wlGenerator.generateWordlists(co);
+
+	}
+
+}
--- src/models/WordListGenerator.java
+++ src/models/WordListGenerator.java
@@ -1,7 +1,7 @@
 /**
  * 
  */
-package experiments;
+package models;
 
 import java.io.BufferedWriter;
 import java.io.File;
@@ -11,7 +11,6 @@ import java.io.OutputStreamWriter;
 import java.io.Writer;
 import java.util.Properties;
 
-import models.Corpus;
 import models.Review.Aspect;
 import weka.core.Attribute;
 import weka.core.FastVector;
@@ -26,49 +25,26 @@ import weka.filters.unsupervised.attribute.StringToWordVector;
 
 /**
  * @author Michi
+ * Generates wordlists and underlying word vector as files on disk
  * 
  */
 public class WordListGenerator {
 
-	/**
-	 * Maximum number of reviews to load
-	 */
-	public static int topX = 5000;
+	
+	public WordListGenerator(String outputDir) {
+		this.outputDir = outputDir;
+	}
+	
+	private String outputDir;
 
 	/**
-	 * @param args
-	 * @throws Exception
+	 * Generates aspect based word lists from the given corpus
+	 * @param co
+	 * @return A String containing the path to all wordlists separated by ";"
 	 */
-	@SuppressWarnings({ "deprecation", "unchecked" })
-	public static void main(String[] args) throws Exception {
-
-		// Setup properties here
-		Properties props = new Properties();
-		props.put("idfTransform", "true");
-		props.put("tfTransform", "true");
-		props.put("outputWordCounts", "true");
-		props.put("lowerCaseTokens", "true");
-		props.put("useStoplist", "true");
-		props.put("stopwordsFile", "data/english-stop-words-small.txt");
-		props.put("wordsToKeep", 2000);
-		props.put("minTermFreq", 3);
-
-		// Affects stanford core
-		props.put("posToKeep", "NN ADJ");// TODO not implemented yet (Marc) See
-											// Review.analyze()
-		props.put("useLemma", "true");// TODO:Check if lemma actually working
-		props.put("includePOS", "false");// TODO: Will mess up word vector
-											// creation
-		props.put("annotators", "tokenize, ssplit, pos, lemma");
-		Corpus co = new Corpus(props);
-
-		System.out.println(props.toString().replaceAll(", ", "\n"));
-
+	@SuppressWarnings({ "deprecation", "unchecked", "rawtypes" })
+	public String generateWordlists(Corpus co) {
 		long startTime = System.currentTimeMillis();
-
-		int size = co.loadFromFile("data/ratebeer.txt", topX);
-		System.out.println("Total amount of reviews loaded: " + size);
-
 		// Set up weka word vector
 		FastVector attributes;
 		Instances dataSet;
@@ -115,17 +91,17 @@ public class WordListGenerator {
 		Instances dataFiltered = transformToWordVector(dataSet, co.getProps());
 
 		// System.out.println(dataFiltered.toString());
-		writeWordlists(dataFiltered, "data/output/wordlists/");
+		String pathsToLists = writeWordlists(dataFiltered);
 
-		writeArffFile(dataFiltered, "data/output/wordvector.arff");
+		writeArffFile(dataFiltered, this.outputDir+"wordvector.arff");
 
 		long stopTime = System.currentTimeMillis();
 		long elapsedTime = stopTime - startTime;
-		System.out.println("Finished in: " + elapsedTime / 1000 + " s");
-
+		System.out.println("Generated wordlists in: " + elapsedTime / 1000 + " s");
+		return pathsToLists;
 	}
 
-	public static Instances transformToWordVector(Instances dataSet, Properties props) {
+	private Instances transformToWordVector(Instances dataSet, Properties props) {
 
 		Instances dataFiltered = null;
 
@@ -149,7 +125,7 @@ public class WordListGenerator {
 		// StringToWordVector(Integer.MAX_VALUE);
 		StringToWordVector filter = new StringToWordVector(wordsToKeep);
 		filter.setIDFTransform(idfTransform);
-		filter.setTFTransform(tfTransform);// TODO change to prop key
+		filter.setTFTransform(tfTransform);
 		filter.setOutputWordCounts(outputWordCounts);
 
 		filter.setMinTermFreq(minTermFreq);
@@ -177,7 +153,7 @@ public class WordListGenerator {
 		return dataFiltered;
 	}
 
-	public static void writeArffFile(Instances data, String fileName) {
+	public void writeArffFile(Instances data, String fileName) {
 		// Write to file
 		try {
 			ArffSaver arffSaverInstance = new ArffSaver();
@@ -186,19 +162,18 @@ public class WordListGenerator {
 			arffSaverInstance.writeBatch();
 			System.out.println("Arff file written to: " + fileName);
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
 
 	/**
-	 * Generates one word list for each aspect
+	 * Writes one word list for each aspect
 	 * 
 	 * @param data
 	 * @return A String containing the path to all wordlists seperated by ";"
 	 */
-	public static String writeWordlists(Instances data, String directory) {
-
+	public String writeWordlists(Instances data) {
+		String result = "";
 		String[] wordLists = new String[4];
 		for (int j = 0; j < wordLists.length; j++) {
 			wordLists[j] = "";
@@ -209,7 +184,7 @@ public class WordListGenerator {
 			double[] scores = data.attributeToDoubleArray(i);
 			double[] aspectScores = new double[4];
 			String token = data.attribute(i).name();
-			System.out.println("Token: " + token);
+//			System.out.println("Token: " + token);
 
 			// Calculate aspect score by summing top and low scores
 			aspectScores[0] = scores[0] + scores[1]; // Aroma
@@ -226,10 +201,10 @@ public class WordListGenerator {
 					// TODO Implement min threshold to include
 				}
 			}
-			System.out.println("Max value and aspect: " + maxIndex + "/" + maxScore);
+//			System.out.println("Max value and aspect: " + maxIndex + "/" + maxScore);
 
 			Double ratio = scores[maxIndex * 2] / scores[maxIndex * 2 + 1];
-			System.out.println("Score ratio: " + ratio);
+//			System.out.println("Score ratio: " + ratio);
 
 			// Word is positive wrt aspect
 			if (ratio > 1) // TODO: Configurable threshold
@@ -238,29 +213,33 @@ public class WordListGenerator {
 			else if (ratio < 1)
 				wordLists[maxIndex] += token + " " + -1 * scores[maxIndex * 2 + 1] + "\n";
 
-			for (int j = 0; j < scores.length; j++) {
-				System.out.print(scores[j] + ",");
-			}
-			System.out.println();
+//			for (int j = 0; j < scores.length; j++) {
+//				System.out.print(scores[j] + ",");
+//			}
+//			System.out.println();
 
-		}//End loop over attributes
-		
+		}// End loop over attributes
+
 		// Write lists to file
 		Writer out = null;
 		try {
 			for (int j = 0; j < wordLists.length; j++) {
 
-				String path = directory;
-				if (j==0) path += Aspect.AROMA.name();
-				if (j==1) path += Aspect.PALATE.name();
-				if (j==2) path += Aspect.TASTE.name();
-				if (j==3) path += Aspect.APPEARANCE.name();
+				String path = this.outputDir;
+				if (j == 0)
+					path += Aspect.AROMA.name();
+				if (j == 1)
+					path += Aspect.PALATE.name();
+				if (j == 2)
+					path += Aspect.TASTE.name();
+				if (j == 3)
+					path += Aspect.APPEARANCE.name();
 				path += ".txt";
-				out = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(path),
-						"UTF-8"));
+				out = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(path), "UTF-8"));
 				out.write(wordLists[j]);
 				out.close();
 				System.out.println("Wordlist written to: " + path + " (" + wordLists[j].split("\n").length + ")");
+				result += path+";";
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
@@ -271,7 +250,7 @@ public class WordListGenerator {
 				e.printStackTrace();
 			}
 		}
-		return "";
+		return result;
 	}
 
 }
